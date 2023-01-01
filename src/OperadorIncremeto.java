e190350eb0c3241e0a7db239fdc7cb35f0586037
Emerson Pontes Cardoso <pontes_saraiva@hotmail.com>
2023-01-01 17:19:45 -0300

    commit

--- src/OperadorIncremeto.java
+++ src/OperadorIncremeto.java
@@ -3,6 +3,11 @@ public class OperadorIncremento {
   public static void main(String[] args) {
     int totalDownloads = 10;
 
+    //totalDownloads  =  totalDownloads + 1;
+    //totalDownloads += 1;
+
+    totalDownloads++;
+
     System.out.println("Total de downloads: " + totalDownloads);
   }
 }
