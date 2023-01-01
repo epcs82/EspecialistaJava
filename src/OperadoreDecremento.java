7986c85c258548ec541463287f6883a0fd0cc90e
Emerson Pontes Cardoso <pontes_saraiva@hotmail.com>
2023-01-01 20:07:32 -0300

    commit

---
+++ src/OperadoreDecremento.java
@@ -0,0 +1,14 @@
+public class OperadorDecremento {
+
+    public static void main(String[]args) {
+
+        int totalDownloads = 90;
+
+        //totalDownloads  =  totalDownloads - 1;
+        //totalDownloads -= 1;
+
+        totalDownloads --1;
+          
+        System.out.println("Total de downloads: " + totalDownloads);
+    }
+}
