5e9459725adcc1473a0100b36f7f653cdd42d482
Emerson Pontes Cardoso <pontes_saraiva@hotmail.com>
2023-01-01 15:42:37 -0300

    aula variaveis

---
+++ src/AulaVariaveis.java
@@ -0,0 +1,14 @@
+package aulavariaveis;
+
+public class AulaVariaveis {
+
+  public static void main(String[] args) {
+    //Louwer Case top
+    int minhaIdade = 40;
+    int suaIdade = 25;
+
+    int totalIdades = minhaIdade + suaIdade;
+
+    System.out.println("A soma das Idades:" + totalIdades);
+  }
+}
