f8efff67b4644492ef160d73dc5a55cacae58fb4
Emerson Pontes Cardoso <pontes_saraiva@hotmail.com>
2023-01-01 16:25:54 -0300

    Operadores

--- src/OperadoresAritmericos.java
+++ src/OperadoresAritmericos.java
@@ -7,8 +7,16 @@ public class OperadoresAritmeticos {
     int minhaIdade = 40;
     int suaIdade = 25;
 
+    //Adição
     int totalIdades = minhaIdade + suaIdade + 100;
-
     System.out.println("A soma das Idades:" + totalIdades);
+
+    //Subtração
+    int diferencaIdades = minhaIdade - suaIdade;
+    System.println("Diferença das idades:" + diferencaIdades);
+
+    //Multiplicação
+    int dobroDaIdade = 2 * suaIdade;
+    System.println("Sobro da sua idade:" + dobroDaIdade);
   }
 }
