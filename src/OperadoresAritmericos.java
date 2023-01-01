a1b0cd0f3c3d5a20a495a05df92e66f2ff83517d
Emerson Pontes Cardoso <pontes_saraiva@hotmail.com>
2023-01-01 16:19:55 -0300

    OperadoresAritmeticos

---
+++ src/OperadoresAritmericos.java
@@ -0,0 +1,14 @@
+package operadoresaritmeticos;
+
+public class OperadoresAritmeticos {
+
+  public static void main(String[] args) {
+    //LouwerCameCase
+    int minhaIdade = 40;
+    int suaIdade = 25;
+
+    int totalIdades = minhaIdade + suaIdade + 100;
+
+    System.out.println("A soma das Idades:" + totalIdades);
+  }
+}
