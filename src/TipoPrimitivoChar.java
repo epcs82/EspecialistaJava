853e4c9722d105315808fc7592132f9d196179f0
Emerson Pontes Cardoso <pontes_saraiva@hotmail.com>
2023-01-03 19:15:16 -0300

    PrimitivoChar

---
+++ src/TipoPrimitivoChar.java
@@ -0,0 +1,12 @@
+public class Char {
+
+  public static void main(String[] args) {
+    char inicialNome = 'E';
+    char inicialSimbolo = '@';
+    char tipoCleinte = '2';
+
+    System.outprinln(inicialNome);
+    System.outprinln(inicialSimbolo);
+    System.outprinln(tipoCleinte);
+  }
+}
