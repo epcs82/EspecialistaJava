a9a250e1569a8e9bb128794c875b594e87411a29
Emerson Pontes Cardoso <pontes_saraiva@hotmail.com>
2023-01-03 18:48:01 -0300

    commit

---
+++ src/OperadorDecremento.java
@@ -0,0 +1,17 @@
+public class OperadorDecremento {
+
+  public static void main(String[] args) {
+    int limiteTentativasLogin = 10;
+
+    //limiteTentativasLogin--;
+    //int novoLimiteTentativasLogin = limiteTentativasLogin--; //metodo pós fixada
+    int novoLimiteTentativasLogin = --limiteTentativasLogin; //metodo pré fixada
+
+    System.out.println(
+      "Limite de tentativas de Login: " + limiteTentativasLogin
+    );
+    System.out.println(
+      "Novo Limite de tentativas de Login: " + limiteTentativasLogin
+    );
+  }
+}
