ea72b70059251b74ae628c3f1bd4c027991e1c39
Emerson Pontes Cardoso <pontes_saraiva@hotmail.com>
2023-01-03 18:55:05 -0300

    commit

---
+++ src/TipoPrimitivoBoolean.java
@@ -0,0 +1,12 @@
+public class tipoPrimitivoBoolean {
+
+  public static void main(String[] args) {
+    boolean compraAprovada = true;
+
+    boolean clienteBloqueado = false;
+
+    System.out.println(compraAprovada);
+
+    System.out.println(clienteBloqueado);
+  }
+}
