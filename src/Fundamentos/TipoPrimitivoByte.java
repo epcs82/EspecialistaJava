3de358c3fc32c6773271c945056a8a10521bf368
Emerson Pontes Cardoso <pontes_saraiva@hotmail.com>
2023-01-03 19:24:03 -0300

    PrimitivoShort

---
+++ src/Fundamentos/TipoPrimitivoByte.java
@@ -0,0 +1,11 @@
+public class Byte {
+
+  public static void main(String[] args) {
+    byte idade = 127; //valor maximo para variavel tipo short
+    //byte idade = 128; //não compila
+
+    short quantidadeEstoque = 32767; //valor maximo para variavel tipo short
+    //short quantidadeEstoque = 32787; //não compila
+
+  }
+}
