e51710ac29be2d1b8daed64bcaa7535c9303090b
Emerson Pontes Cardoso <pontes_saraiva@hotmail.com>
2023-01-03 20:14:09 -0300

    Correção

--- src/Fundamentos/TipoPrimitivoLong.java
+++ src/Fundamentos/TipoPrimitivoLong.java
@@ -3,5 +3,8 @@ public class Long {
   public static void main(String[] args) {
     long pupulacaoGoiania = 699_097;
     long pupulacaoGoiania2 = 2_147_483_648L;
+
+    //System.out.println(2_147_483_648);//não compila
+    System.out.println(2_147_483_648L);
   }
 }
