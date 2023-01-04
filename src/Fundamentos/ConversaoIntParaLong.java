1276eafdcd2bddda6093e647fdc81f283691c918
Emerson Pontes Cardoso <pontes_saraiva@hotmail.com>
2023-01-03 21:03:31 -0300

    ConversaoIntParaLong

---
+++ src/Fundamentos/ConversaoIntParaLong.java
@@ -0,0 +1,12 @@
+public class ConversaoIntParaLong {
+
+    public static main(String[] args) {
+
+        int y = 102344;
+
+        long x = (int) y;//conversão implicita
+    
+      }
+      
+    }
+    
